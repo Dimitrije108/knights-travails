92380af507be77bf8e88413c450af37267b84643
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-07-28 14:08:14 +0200

    feat: add knight moves logic that adds all possible moves for the given node
    Add moves method will add all possible knight moves keeping true to board bounds.

--- KnightsTravails.mjs
+++ KnightsTravails.mjs
@@ -5,6 +5,31 @@ class Node {
     this.data = data;
     this.moves = [];
   }
+  // Check for out of bounds move
+  inBounds(value) {
+    return value < 0 || value > 7 ? false : true;
+  }
+  // Add all possible knight moves
+  addMoves() {
+    const row = this.data[0];
+    const column = this.data[1];
+    const moves = [
+      [1, 2],
+      [1, -2],
+      [-1, 2],
+      [-1, -2],
+      [2, 1],
+      [2, -1],
+      [-2, 1],
+      [-2, -1],
+    ];
+
+    moves.forEach(([i, j]) => {
+      if (this.inBounds(row + i) && this.inBounds(column + j)) {
+        this.moves.push([row + i, column + j]);
+      }
+    });
+  }
 }
 
 class ChessBoard {
@@ -22,7 +47,19 @@ class ChessBoard {
     }
     return arr;
   }
+
+  find(value) {
+    for (let node of this.board) {
+      if (JSON.stringify(node.data) === JSON.stringify(value)) {
+        return node;
+      }
+    }
+    return "Value not found";
+  }
 }
 
 const test = new ChessBoard();
 console.log(test.board);
+const testMoves = test.find([4, 7]);
+testMoves.addMoves();
+console.log(testMoves.moves);
