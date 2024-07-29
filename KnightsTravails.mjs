61e94ee7c12cd08fad9d13a01bd13e4d4fecc589
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-07-29 12:11:35 +0200

    feat: add knight moves functionality that will take in a start and end position and print out all the moves to get from start to end
    Knight moves will print all moves done via breadth-first search algorithm and return the result path.

--- KnightsTravails.mjs
+++ KnightsTravails.mjs
@@ -3,7 +3,7 @@
 class Node {
   constructor(data) {
     this.data = data;
-    this.moves = [];
+    this.moves = this.addMoves();
   }
   // Check for out of bounds move
   inBounds(value) {
@@ -11,6 +11,7 @@ class Node {
   }
   // Add all possible knight moves
   addMoves() {
+    const arr = [];
     const row = this.data[0];
     const column = this.data[1];
     const moves = [
@@ -26,9 +27,10 @@ class Node {
 
     moves.forEach(([i, j]) => {
       if (this.inBounds(row + i) && this.inBounds(column + j)) {
-        this.moves.push([row + i, column + j]);
+        arr.push([row + i, column + j]);
       }
     });
+    return arr;
   }
 }
 
@@ -38,28 +40,67 @@ class ChessBoard {
   }
 
   makeBoard() {
-    let arr = [];
+    const arr = [];
     for (let i = 0; i <= 7; i++) {
       for (let j = 0; j <= 7; j++) {
-        let square = new Node([i, j]);
+        const square = new Node([i, j]);
         arr.push(square);
       }
     }
     return arr;
   }
-
+  // Compare two squares/fields
+  compareValues(first, second) {
+    if (JSON.stringify(first) === JSON.stringify(second)) {
+      return true;
+    }
+    return false;
+  }
+  // Return node if value is present
   find(value) {
     for (let node of this.board) {
-      if (JSON.stringify(node.data) === JSON.stringify(value)) {
+      if (this.compareValues(node.data, value)) {
         return node;
       }
     }
-    return "Value not found";
+    return false;
   }
 }
 
-const test = new ChessBoard();
-console.log(test.board);
-const testMoves = test.find([4, 7]);
-testMoves.addMoves();
-console.log(testMoves.moves);
+const chessboard = new ChessBoard();
+
+const knightMoves = (start, end) => {
+  const startNode = chessboard.find(start);
+
+  const queue = [startNode];
+  const visited = [];
+
+  while (queue.length > 0) {
+    const node = queue.shift();
+    // Save the node to visited nodes
+    visited.push(node);
+    // Check if move value is the end value
+    if (chessboard.compareValues(node.data, end)) {
+      console.log(
+        `You made it in ${visited.length - 1} move(s)! Here's your path:`
+      );
+      visited.forEach((node) => console.log(node.data));
+      return;
+    }
+    // Add possible moves to the queue
+    node.moves.forEach((move) => {
+      const newNode = chessboard.find(move);
+      if (!visited.includes(newNode)) {
+        queue.push(newNode);
+      }
+    });
+  }
+};
+
+console.log(knightMoves([0, 0], [3, 3]));
+
+// const test = new ChessBoard();
+// console.log(test.board);
+// const testMoves = test.find([4, 7]);
+// testMoves.addMoves();
+// console.log(testMoves.moves);
